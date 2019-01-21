803d73a3d61a58eccf173429ce0da94e87ac1c11
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-01-21 16:51:36 -0500

    added server runner class

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,23 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String args[] ) throws Exception {
-        final ServerSocket server = new ServerSocket(5000);
-        System.out.println("Listening for connection on port 5000 ....");
-        while (true) {
-            final Socket clientSocket = server.accept();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr); String line = reader.readLine();
-            clientSocket.getOutputStream().write(("hello world").getBytes("UTF-8"));
-            while (!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
-            }
+    String host;
+    static int port;
+    ServerSocket serverSocket;
 
+    public Server(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
 
+    public void start() throws IOException {
+        serverSocket = new ServerSocket(port);
+        while(true) {
+//            do smth
         }
+    }
+
+    public void stop() throws IOException {
+        serverSocket.close();
     }
 }
