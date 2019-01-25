e85313ca3a9d3efb93a29532c1931bdd43018d2e
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-01-25 13:49:00 -0500

    Refactor tests for Request and ResponseGenerator classes

--- src/test/java/com/olgaboiar/mint/ResponseGeneratorTest.java
+++ src/test/java/com/olgaboiar/mint/ResponseGeneratorTest.java
@@ -6,11 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ResponseGeneratorTest {
     @Test
-    void testResponseGeneratorReturnsResponseArray() {
+    void testResponseGeneratorReturnsResponseObject() {
         ResponseGenerator testResponseGenerator = new ResponseGenerator();
         Response response = testResponseGenerator.generateResponse();
         String[] responseArray = {response.getStatusLine(), response.getContentType(), ""};
         assertArrayEquals(new String[]{"HTTP/1.0 200 OK", "Content-Type: text/html", ""}, responseArray);
+    }
+
+    @Test
+    void testResponseGeneratorReturnsResponseForNotFound() {
+        ResponseGenerator testResponseGenerator = new ResponseGenerator();
+        Response response = testResponseGenerator.generateNotFoundResponse();
+        String[] responseArray = {response.getStatusLine(), response.getContentType(), ""};
+        assertArrayEquals(new String[]{"HTTP/1.0 404 Not Found", "Content-Type: text/plain", ""}, responseArray);
     }
 
 }
