00c8feb04f6c8f6c322341fc12eb4405e3d64a2e
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-01-30 14:46:27 -0500

    Remove date from constants

--- src/main/java/com/olgaboiar/mint/Constants.java
+++ src/main/java/com/olgaboiar/mint/Constants.java
@@ -12,6 +12,4 @@ public final class Constants {
     public static final String STATUS_CODE_404 = "404 Not Found";
 
     public static final String PROTOCOL = "HTTP/1.1";
-
-    public static final String DATE = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
 }
--- src/main/java/com/olgaboiar/mint/HeaderGenerator.java
+++ src/main/java/com/olgaboiar/mint/HeaderGenerator.java
@@ -1,13 +1,16 @@
 package com.olgaboiar.mint;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static com.olgaboiar.mint.Constants.*;
 
 public class HeaderGenerator {
-
+    String currentDate = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
 
     public Header generate(String statusCode, String contentType) {
         String statusLine = PROTOCOL + " " + statusCode;
-        String date = "Date: " + DATE;
+        String date = "Date: " + currentDate;
         Header header = new Header(statusLine, contentType, date);
         return header;
     }
