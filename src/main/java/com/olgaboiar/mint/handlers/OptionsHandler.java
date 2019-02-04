2839a21214c976be9087a6437ece4da694bfac27
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-02-04 10:41:05 -0500

    Add Options handler

---
+++ src/main/java/com/olgaboiar/mint/handlers/OptionsHandler.java
@@ -0,0 +1,27 @@
+package com.olgaboiar.mint.handlers;
+
+import com.olgaboiar.mint.Request;
+import com.olgaboiar.mint.Response;
+import com.olgaboiar.mint.ResponseGenerator;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static com.olgaboiar.mint.Constants.STATUS_CODE_200;
+
+public class OptionsHandler implements IHandler {
+    Set<String> allowedMethods;
+    Response response;
+
+    public OptionsHandler(Set<String> allowedMethods) {
+        this.allowedMethods = allowedMethods;
+    }
+
+    @Override
+    public Response handleRequest(Request request) throws IOException {
+        String body = "";
+        response =  new ResponseGenerator().generateResponse(STATUS_CODE_200, body);
+        response.getHeader().setAllowMethods(allowedMethods);
+        return response;
+    }
+}
