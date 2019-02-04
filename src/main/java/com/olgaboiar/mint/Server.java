20be29df16e9722f462932066b30db43d90600ec
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-02-04 16:44:42 -0500

    Extract from readClientInput method

--- src/main/java/com/olgaboiar/mint/Server.java
+++ src/main/java/com/olgaboiar/mint/Server.java
@@ -19,7 +19,7 @@ public class Server {
     static int port;
     RequestParser parser = new RequestParser();
     Router router = new Router();
-    Response response;
+//    Response response;
     ILogger logger;
     static String date = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
 
@@ -39,7 +39,9 @@ public class Server {
     public void run() throws IOException {
         acceptClientConnection();
         listenToClientConnection();
-        readClientInput();
+        List<String> clientInput = readClientInput();
+        Request parsedRequest = parseRequest(clientInput);
+        Response response = prepareResponse(parsedRequest);
         sendResponseToClient(response);
         closeClientConnection();
     }
@@ -50,22 +52,32 @@ public class Server {
 
     private void listenToClientConnection() throws IOException {
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        out = new PrintWriter(clientSocket.getOutputStream());
+//        out = new PrintWriter(clientSocket.getOutputStream());
     }
 
-    private void readClientInput() throws IOException {
-        List<String> list = new ArrayList<String>();
+    private List<String> readClientInput() throws IOException {
+        List<String> clientInput = new ArrayList<String>();
         String input = in.readLine();
         while (input.length() > 0) {
-            list.add(input);
+            clientInput.add(input);
             input = in.readLine();
         }
-        Request currentRequest = parser.parse(list);
-        logger.logMessage("\nReceived request:\n" + list);
-        response = router.route(currentRequest);
+        return clientInput;
+    }
+
+    private Request parseRequest(List<String> clientInput) throws IOException {
+        Request currentRequest = parser.parse(clientInput);
+        logger.logMessage("\nReceived request:\n" + clientInput);
+        return currentRequest;
+    }
+
+    private Response prepareResponse(Request parsedRequest) throws IOException {
+        Response response = router.route(parsedRequest);
+        return response;
     }
 
     private void sendResponseToClient(Response response) throws IOException {
+        out = new PrintWriter(clientSocket.getOutputStream());
         out.println(response.prepareResponse());
         logger.logMessage("\nResponse sent:\n" + response.prepareResponse());
         out.flush();
