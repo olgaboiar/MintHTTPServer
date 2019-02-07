cde8f988f9b87510b787039c2b8ada22d55c8d69
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-02-07 14:23:09 -0500

    Create interface for ServerConnection

--- src/main/java/com/olgaboiar/mint/ServerConnection.java
+++ src/main/java/com/olgaboiar/mint/ServerConnection.java
@@ -9,23 +9,27 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ServerConnection {
+public class ServerConnection implements IServerConnection {
     ServerSocket serverSocket;
 
+    @Override
     public void createServerSocket(int port) throws IOException {
         serverSocket = new ServerSocket(port);
     }
 
+    @Override
     public Socket acceptClientConnection() throws IOException {
         Socket clientSocket = serverSocket.accept();
         return clientSocket;
     }
 
+    @Override
     public BufferedReader listenToClientConnection(Socket clientSocket) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         return in;
     }
 
+    @Override
     public List<String> readClientInput(BufferedReader in) throws IOException {
         List<String> clientInput = new ArrayList<String>();
         String input = in.readLine();
@@ -36,20 +40,24 @@ public class ServerConnection {
         return clientInput;
     }
 
+    @Override
     public void closeClientConnection(BufferedReader in, PrintWriter out, Socket clientSocket) throws IOException {
         out.close();
         in.close();
         clientSocket.close();
     }
 
+    @Override
     public void stop() throws IOException {
         serverSocket.close();
     }
 
-    private PrintWriter createOutPutStream(Socket clientSocket) throws IOException {
+    @Override
+    public PrintWriter createOutPutStream(Socket clientSocket) throws IOException {
         return new PrintWriter(clientSocket.getOutputStream());
     }
 
+    @Override
     public PrintWriter sendResponseToClient(Response response, Socket clientSocket) throws IOException {
         PrintWriter out = createOutPutStream(clientSocket);
         out.println(response.prepareResponse());
